a9ec274c7acd36810b8d505da4084946fdeeff32
Daniil <rychenkov16@yandex.ru>
2024-07-28 21:04:45 +0300

    add switch case unit 4.1

--- Program.cs
+++ Program.cs
@@ -1,34 +1,28 @@
-﻿using System;
-
-class MainClass
+﻿switch (Console.ReadLine())
 {
-    public static void Main(string[] args)
-    {
-        Console.WriteLine("Напишите свой любимый цвет на английском с маленькой буквы");
+    case "red":
+        Console.BackgroundColor = ConsoleColor.Red;
+        Console.ForegroundColor = ConsoleColor.Black;
 
-        var color = Console.ReadLine();
+        Console.WriteLine("Your color is red!");
+        break;
 
-        if (color == "red")
-        {
-            Console.BackgroundColor = ConsoleColor.Red;
-            Console.ForegroundColor = ConsoleColor.Black;
+    case "green":
+        Console.BackgroundColor = ConsoleColor.Green;
+        Console.ForegroundColor = ConsoleColor.Black;
 
-            Console.WriteLine("Your color is red!");
-        }
+        Console.WriteLine("Your color is green!");
+        break;
+    case "cyan":
+        Console.BackgroundColor = ConsoleColor.Cyan;
+        Console.ForegroundColor = ConsoleColor.Black;
 
-        else if (color == "green")
-        {
-            Console.BackgroundColor = ConsoleColor.Green;
-            Console.ForegroundColor = ConsoleColor.Black;
+        Console.WriteLine("Your color is cyan!");
+        break;
+    default:
+        Console.BackgroundColor = ConsoleColor.Yellow;
+        Console.ForegroundColor = ConsoleColor.Red;
 
-            Console.WriteLine("Your color is green!");
-        }
-        else
-        {
-            Console.BackgroundColor = ConsoleColor.Cyan;
-            Console.ForegroundColor = ConsoleColor.Black;
-
-            Console.WriteLine("Your color is cyan!");
-        }
-    }
+        Console.WriteLine("Your color is yellow!");
+        break;
 }
