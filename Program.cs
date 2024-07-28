093aecf412398f0def020fdfa2a4bb929fe49e9b
Daniil <rychenkov16@yandex.ru>
2024-07-28 16:02:53 +0300

    добавил анкету

--- Program.cs
+++ Program.cs
@@ -5,11 +5,15 @@ class MainClass
     public static void Main(string[] args)
     {
 
-        string myName;
-        myName = "Dan";
+        string MyName = "Daniil";
+        byte MyAge = 23;
+        bool HaveIApet = false;
+        double MyShoeSize = 42.5;
 
-        Console.WriteLine(myName);
+        Console.WriteLine("My name is " + MyName);
+        Console.WriteLine("MyAge " + MyAge);
+        Console.WriteLine("Do I have a pet? " + HaveIApet);
+        Console.WriteLine("My shoe size is " + MyShoeSize);
 
-        Console.ReadKey();
     }
 }
